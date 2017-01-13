e7d5d8e157347768053de1a2822c1576ee8841f1
ajith <ajith.kjames3@gmail.com>
2017-01-13 18:32:48 +0530

    sds

--- stamp/settings.py
+++ stamp/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '7v(p!m%m99e=bfbkh85o8of^(n^s+50*6^$0ltyfu5zakehd6l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
